2c5183a82eaa69b1f3f5cc2a9de4117bf6e26d19
ManatoMaTsumoto <manakyun@outlook.jp>
2024-09-02 18:52:37 +0900

    OSCCoreをプロジェクトへインポート

---
+++ Assets/Resource/System/Network/OSCManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCManager : MonoBehaviour
+{
+    //1フレーム前の全プレイヤーデータ格納用
+    List<Player> localPlayerData = new List<Player>();
+
+    //
+    //List<Player> localPlayerData = new List<Player>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
