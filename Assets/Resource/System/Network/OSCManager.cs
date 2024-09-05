06c98defbea2318a4aac28d75d06cb1fe9ded43b
ManatoMaTsumoto <manakyun@outlook.jp>
2024-09-05 12:22:59 +0900

    送信データ用構造体とバイト変換

--- Assets/Resource/System/Network/OSCManager.cs
+++ Assets/Resource/System/Network/OSCManager.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OscCore;
 
 public class OSCManager : MonoBehaviour
 {
-    //1フレーム前の全プレイヤーデータ格納用
-    List<Player> localPlayerData = new List<Player>();
+    //////////////////////////////
+    //////// 本番使用変数 ////////
+    //////////////////////////////
 
-    //
-    //List<Player> localPlayerData = new List<Player>();
+    List<SendDataCreator.PlayerNetData> netData = new List<SendDataCreator.PlayerNetData>();
+
+    //1フレーム前の全プレイヤーデータ格納用
+    List<SendDataCreator.PlayerNetData> beforeNetData = new List<SendDataCreator.PlayerNetData>();
+
+
+    ///////// OSCcore周り ////////
+
+    OscClient client;
+    OscServer server;
+
+
+    ////////////////////////////////
+    //////// デバック用変数 ////////
+    ////////////////////////////////
+
+    //現フレームの全プレイヤーデータ格納用
+    //List<Player> nowFramePlayerData = new List<Player>();
+
+    //////////////////////
+    //////// 関数 ////////
+    //////////////////////
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //netData[0].mainPacketData
     }
 
     // Update is called once per frame
