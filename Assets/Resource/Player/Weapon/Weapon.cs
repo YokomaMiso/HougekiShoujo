e84e65c2f64747a7f1a0ed30683e476380512ae5
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-09-06 18:26:01 +0900

    Update: WeaponクラスをSubWeaponクラスに変更

--- Assets/Resource/Player/Weapon/Weapon.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "WeaponData", menuName = "Create/PlayerData/WeaponData", order = 1)]
-public class Weapon : ScriptableObject
-{
-    [SerializeField, Header("Weapon Graphic")] Sprite weaponGraphic;
-}
