86a7d68f33f745037975d119896a93327d9a5a0c
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-10-23 19:18:03 +0900

    Fix: 名前入力処理のバグを修正

--- Assets/Resource/UI/TitleCanvas/InputName.cs
+++ Assets/Resource/UI/TitleCanvas/InputName.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -22,18 +24,93 @@ public class InputName : MonoBehaviour
     {
         if (parent.GetTitleState() != TITLE_STATE.INPUT_NAME) { return; }
 
-        if (InputManager.GetKeyDown(BoolActions.SouthButton))
+        inputField.Select();
+
+        AddText();
+        SubText();
+        if (Keyboard.current[Key.Enter].wasPressedThisFrame) { Submit(); }
+        else if (Keyboard.current[Key.Escape].wasPressedThisFrame) { Cancel(); }
+
+        //if (InputManager.GetKeyDown(BoolActions.SouthButton)) { }
+        //else if (InputManager.GetKeyDown(BoolActions.EastButton)) { }
+    }
+    void AddText()
+    {
+        int charNum;
+        if (Keyboard.current[Key.LeftShift].isPressed || Keyboard.current[Key.RightShift].isPressed) { charNum = 'A'; }
+        else { charNum = 'a'; }
+
+        for (int i = 0; i < 'z' + 1 - 'a'; i++)
         {
-            string playerName = inputField.text;
-            if (playerName == "") { playerName = "Player " + (Managers.instance.playerID + 1).ToString(); }
-            Managers.instance.optionData.playerName = playerName;
-            Managers.instance.SaveOptionData(Managers.instance.optionData);
-            OSCManager.OSCinstance.roomData.playerName = playerName;
-            parent.ChangeTitleState(TITLE_STATE.CHANGE_TO_CONNECTION);
+            char c = (char)(charNum + i);
+            string t = "";
+            t += c;
+
+            if (Keyboard.current.FindKeyOnCurrentKeyboardLayout(t).wasPressedThisFrame)
+            {
+                AddTextToInputField(t);
+            }
         }
-        else if (InputManager.GetKeyDown(BoolActions.EastButton))
+
+        charNum = '0';
+        for (int i = 0; i < '9' + 1 - '0'; i++)
         {
-            parent.ChangeTitleState(TITLE_STATE.SELECT);
+            char c = (char)(charNum + i);
+            string t = "";
+            t += c;
+
+            if (Keyboard.current.FindKeyOnCurrentKeyboardLayout(t).wasPressedThisFrame)
+            {
+                AddTextToInputField(t);
+            }
         }
+    }
+    void AddTextToInputField(string _c)
+    {
+        if (inputField.text.Length >= inputField.characterLimit) { return; }
+        inputField.text += _c;
+    }
+    void SubText()
+    {
+        if (inputField.text.Length == 0) { return; }
+
+        if (Keyboard.current[Key.Backspace].wasPressedThisFrame)
+        {
+            string t = inputField.text.Remove(inputField.text.Length - 1);
+            inputField.text = t;
+        }
+    }
+
+    void Submit()
+    {
+        //決定ボタンが押されてないなら
+        if (!InputManager.GetKeyDown(BoolActions.SouthButton))
+        {
+            //エンターキーが押されてないならリターン
+            if (!Keyboard.current[Key.Enter].wasPressedThisFrame) { return; }
+        }
+
+        if (Keyboard.current.FindKeyOnCurrentKeyboardLayout("z").wasPressedThisFrame) { return; }
+        if (Keyboard.current.FindKeyOnCurrentKeyboardLayout("j").wasPressedThisFrame) { return; }
+
+        string playerName = inputField.text;
+        if (playerName == "") { playerName = "Player " + (Managers.instance.playerID + 1).ToString(); }
+        Managers.instance.optionData.playerName = playerName;
+        Managers.instance.SaveOptionData(Managers.instance.optionData);
+        OSCManager.OSCinstance.roomData.playerName = playerName;
+        parent.ChangeTitleState(TITLE_STATE.CHANGE_TO_CONNECTION);
+    }
+
+    void Cancel()
+    {
+        //キャンセルボタンが押されてないなら
+        if (InputManager.GetKeyDown(BoolActions.EastButton)) { return; }
+
+        if (Keyboard.current.FindKeyOnCurrentKeyboardLayout("k").wasPressedThisFrame) { return; }
+        if (Keyboard.current.FindKeyOnCurrentKeyboardLayout("x").wasPressedThisFrame) { return; }
+        if (Keyboard.current[Key.LeftShift].wasPressedThisFrame) { return; }
+        if (Keyboard.current[Key.RightShift].wasPressedThisFrame) { return; }
+
+        parent.ChangeTitleState(TITLE_STATE.SELECT);
     }
 }
