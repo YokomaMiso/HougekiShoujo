4b9098f25a031a586438d0ba4833252fa05951a3
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-12-03 17:22:01 +0900

    Add: チュートリアルの追加

--- Assets/Resources/System/OptionData.cs
+++ Assets/Resources/System/OptionData.cs
@@ -9,11 +9,12 @@ public class OptionData : ScriptableObject
     [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
     public string playerName = "Player";
     public float masterVolume = 1.0f;
-    public float bgmVolume = 0.3f;
-    public float sfxVolume = 0.9f;
-    public float voiceVolume = 0.8f;
+    public float bgmVolume = 0.5f;
+    public float sfxVolume = 1.0f;
+    public float voiceVolume = 1.0f;
     public bool cameraShakeOn = true;
     public float mortarSensitive = 20.0f;
+    public bool displayTutorial = true;
     public int languageNum = 0;
 
     public void Init(OptionData _od)
@@ -25,6 +26,7 @@ public class OptionData : ScriptableObject
         voiceVolume = _od.voiceVolume;
         cameraShakeOn = _od.cameraShakeOn;
         mortarSensitive = _od.mortarSensitive;
+        displayTutorial = _od.displayTutorial;
         languageNum = _od.languageNum;
     }
 }
