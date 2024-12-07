e33c6ba2c1febd249f16456c2e179448f74b9e93
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-12-08 00:04:59 +0900

    Update: チュートリアルの更新

--- Assets/Resources/UI/TitleCanvas/Tutorial/Tutorial_4/TutorialFourBehavior.cs
+++ Assets/Resources/UI/TitleCanvas/Tutorial/Tutorial_4/TutorialFourBehavior.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class TutorialFourBehavior : MonoBehaviour
 {
     [SerializeField] GameObject[] texts;
+
     void Start()
     {
         Instantiate(texts[(int)Managers.instance.nowLanguage], transform);
@@ -24,16 +25,36 @@ public class TutorialFourBehavior : MonoBehaviour
     void ChangeDisplayButtons()
     {
         Sprite[] applySprites = InputManager.nowButtonSpriteData.GetAllSprites();
-        transform.GetChild(0).GetComponent<Image>().sprite = applySprites[4];
-        transform.GetChild(1).GetComponent<Image>().sprite = applySprites[6];
-        transform.GetChild(2).GetComponent<Image>().sprite = applySprites[7];
-        transform.GetChild(3).GetComponent<Image>().sprite = applySprites[6];
 
         Color color;
-        if (InputManager.currentController == ControllerType.Keyboard) { color = Color.clear; }
-        else { color = Color.white; }
+        if (InputManager.currentController == ControllerType.Keyboard)
+        {
+            color = Color.clear;
+            transform.GetChild(0).GetComponent<Image>().color = color;
+            transform.GetChild(1).GetComponent<Image>().color = color;
+            transform.GetChild(2).GetComponent<Image>().color = color;
+            transform.GetChild(3).GetComponent<Image>().color = color;
 
-        transform.GetChild(0).GetComponent<Image>().color = color;
-        transform.GetChild(2).GetComponent<Image>().color = color;
+            color = Color.white;
+            transform.GetChild(4).GetComponent<Image>().color = color;
+            transform.GetChild(5).GetComponent<Image>().color = color;
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Image>().sprite = applySprites[4];
+            transform.GetChild(1).GetComponent<Image>().sprite = applySprites[6];
+            transform.GetChild(2).GetComponent<Image>().sprite = applySprites[7];
+            transform.GetChild(3).GetComponent<Image>().sprite = applySprites[6];
+
+            color = Color.white;
+            transform.GetChild(0).GetComponent<Image>().color = color;
+            transform.GetChild(1).GetComponent<Image>().color = color;
+            transform.GetChild(2).GetComponent<Image>().color = color;
+            transform.GetChild(3).GetComponent<Image>().color = color;
+
+            color = Color.clear;
+            transform.GetChild(4).GetComponent<Image>().color = color;
+            transform.GetChild(5).GetComponent<Image>().color = color;
+        }
     }
 }
