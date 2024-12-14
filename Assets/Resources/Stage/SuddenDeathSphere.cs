3378788c1c2e2f01a75b79e19ce48179ee3892d4
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-12-14 21:51:44 +0900

    Add: サドンデスエリアにSFXを鳴らす機能を追加

--- Assets/Resources/Stage/SuddenDeathSphere.cs
+++ Assets/Resources/Stage/SuddenDeathSphere.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class SuddenDeathSphere : MonoBehaviour
 {
+    [SerializeField] AudioClip damageSFX;
+
     void OnTriggerExit(Collider other)
     {
-        Player player= other.GetComponent<Player>();
+        Player player = other.GetComponent<Player>();
         if (!player) { return; }
         if (player.GetPlayerID() != Managers.instance.playerID) { return; }
 
         player.SetDead(player.GetPlayerID());
+        SoundManager.PlaySFX(damageSFX, player.transform);
     }
 }
