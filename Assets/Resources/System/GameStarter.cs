c7ffe81eff3118826cee1ce8a27e0592e16b3f81
TsunagiSamurai <108010157+TsunagiSamurai@users.noreply.github.com>
2024-12-18 19:27:14 +0900

    Add: 翼アニメ

--- Assets/Resources/System/GameStarter.cs
+++ Assets/Resources/System/GameStarter.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class GameStarter : MonoBehaviour
 {
+    [SerializeField] GameObject postProcess;
+
     void Start()
     {
         Managers.instance.gameManager.CreatePlayer();
+
+        postProcess.SetActive(OSCManager.OSCinstance.GetRoomData(0).stageNum == 3);
+
         Destroy(gameObject);
     }
 
